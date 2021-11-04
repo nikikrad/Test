fced6e31d81791dfa73770c6e70cd6931c647f86
NiKiKRaD <nikikrad228@gmail.com>
2021-11-04 13:56:35 +0300

    test

--- app/src/main/java/com/example/myapplication/NewActivity.kt
+++ app/src/main/java/com/example/myapplication/NewActivity.kt
@@ -2,17 +2,28 @@ package com.example.myapplication
 
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
+import androidx.core.view.isVisible
 
 class NewActivity: MainActivity() {
     private lateinit var square: View
+    private lateinit var view: View
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.newactivity)
-        val LOGIN = intent.extras?.getString(LOGIN)
-        val PASSWORD = intent.extras?.getString(PASSWORD)
+        val LOGIN: String? = intent.extras?.getString(LOGIN)
+        val PASSWORD: String? = intent.extras?.getString(PASSWORD)
+        reg(LOGIN, PASSWORD)
     }
 
+    fun reg(login:String? = "default", password: String? = "default"){
+        if (login == "admin" || password == "admin")
+            view = findViewById(R.id.v_viewInfo)
 
+
+    }
 
 }
