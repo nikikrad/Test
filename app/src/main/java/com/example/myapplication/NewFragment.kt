b4e0c63ecd943b52f1614ec9f95307a45c55fbfc
NiKiKRaD <nikikrad228@gmail.com>
2021-11-08 18:10:02 +0300

    test

--- app/src/main/java/com/example/myapplication/NewFragment.kt
+++ app/src/main/java/com/example/myapplication/NewFragment.kt
@@ -1,7 +1,21 @@
 package com.example.myapplication
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.myapplication.databinding.FragmentNewBinding
 
 class NewFragment:Fragment() {
+    private var binding: FragmentNewBinding? = null
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentNewBinding.inflate(inflater,container,false)
+        return binding?.root
+    }
 }
